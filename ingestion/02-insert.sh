2aa22dc51af1ff79c56204420a1e9397916d01f9
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2025-11-15 14:31:16 +0000

    verify

--- ingestion/02-insert.sh
+++ ingestion/02-insert.sh
@@ -11,10 +11,10 @@ databricks api post /api/2.0/sql/statements \
 --profile DEFAULT \
 --json @02-insert.json > /tmp/02-response.json
 
-stmt=$(yq '.statement_id' /tmp/02-response.json)
+stmt=$(yq -r '.statement_id' /tmp/02-response.json)
 
 while true; do
-   state=$(databricks api get /api/2.0/sql/statements/01f0c221-6285-1a82-b449-c1d1e4f51bb5 | yq '.status.state')
+   state=$(databricks api get /api/2.0/sql/statements/$stmt | yq '.status.state')
    if [ "$state" == "PENDING" ]; then
       sleep 1
    else
