55ab8a66428c792e99ef5cff89e6c53c5b5d412a
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2025-11-16 20:47:52 +0000

    select with python

---
+++ ingestion/03-verify.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S uv run
+
+import os
+
+import pandas as pd
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service import sql
+from databricks.sdk.service.sql import StatementState
+from dotenv import load_dotenv
+
+load_dotenv()
+
+warehouse_id = os.getenv("WAREHOUSE_ID")
+
+
+w = WorkspaceClient()
+
+res = w.statement_execution.execute_statement(
+    warehouse_id=warehouse_id,
+    statement="SELECT * FROM dz.dz.data",
+)
+
+# print (res)
+
+if res.result and res.result.data_array:
+    column_names = [col.name for col in res.manifest.schema.columns]
+    df = pd.DataFrame(res.result.data_array, columns=column_names)
+    print(df)
+else:
+    print("No data returned or an error occurred.")
