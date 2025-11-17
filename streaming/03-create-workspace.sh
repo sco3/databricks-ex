d399ff740b40accd19ddb74c9c52a869b968899e
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2025-11-17 17:50:30 +0000

    streaming works

--- streaming/03-create-workspace.sh
+++ streaming/03-create-workspace.sh
@@ -2,6 +2,8 @@
 
 set -xueo pipefail
 
+databricks workspace mkdirs /Users/texter.texel@gmail.com/streaming
+
 databricks workspace import \
   /Users/texter.texel@gmail.com/streaming/streaming.sql \
   --file streaming.sql \
---
+++ streaming/05-verify.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S uv run
+
+import os
+
+import pandas as pd
+from databricks.sdk import WorkspaceClient
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    warehouse_id = os.getenv("WAREHOUSE_ID")
+    w = WorkspaceClient()
+
+    res = w.statement_execution.execute_statement(
+        warehouse_id=warehouse_id,
+        statement="select * from dz.dz.data_streaming;",
+    )
+
+    if res.result and res.result.data_array:
+        column_names = [col.name for col in res.manifest.schema.columns]
+        df = pd.DataFrame(res.result.data_array, columns=column_names)
+        print(df)
+    else:
+        print("No data returned or an error occurred.")
