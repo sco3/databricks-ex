69682f47eb9b52f01d2b0593a1d29b0aee3c1cef
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2025-11-18 15:09:31 +0000

    ws

--- json/json.py
+++ json/json.py
@@ -5,3 +5,16 @@ spark.read.json ('/Volumes/dz/dz/dz-vol-json').display()
 
 files = dbutils.fs.ls("/Volumes/dz/dz/dz-vol-json")
 display(files)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from text.`/Volumes/dz/dz/dz-vol-json`
+
+# COMMAND ----------
+
+
+
+df = spark.read.text("/Volumes/dz/dz/dz-vol-json")
+
+df.display(truncate=False)
